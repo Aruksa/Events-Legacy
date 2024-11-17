45815f9e880b8231f38b6ee5918d7dc03fcee449
Shahriar-Ruposh <shahriarrcse67@gmail.com>
2024-11-17 11:26:21 +0600

    rename the attendy api route
    rename all the attendy api route according to the REST naming convention and remove the attendy route file from back-end

--- server/src/routes/eventRoute.ts
+++ server/src/routes/eventRoute.ts
@@ -1,17 +1,23 @@
 // src/routes/eventRoutes.ts
 
 import express from "express";
-import { createEvent, updateEvent, deleteEvent, getAllEvents, getEventsById, userEvents, searchEvent } from "../controllers/eventController";
+import { createEvent, updateEvent, deleteEvent, getAllEvents, getEventsById, userEvents } from "../controllers/eventController";
+import { createUpdateAttendy, getAllEventAttendy, getAttendy } from "../controllers/attendyController";
 import { authenticateToken } from "../middleware/authMiddleware";
 
 const router = express.Router();
 
 router.get("/", getAllEvents);
 router.get("/:id", getEventsById);
+router.get("/user-event", authenticateToken, userEvents);
 
 router.post("/", authenticateToken, createEvent);
 router.put("/:id", authenticateToken, updateEvent);
 router.delete("/:id", authenticateToken, deleteEvent);
-router.get("/user-event", authenticateToken, userEvents);
+
+// Attendy Routes
+router.get("/:eventId/user-attendy", authenticateToken, getAttendy);
+router.get("/:eventId/attendies", getAllEventAttendy);
+router.post("/:eventId/attendies", authenticateToken, createUpdateAttendy);
 
 export default router;
